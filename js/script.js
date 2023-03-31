44f279fa565dd11336a56a886e4c03ee16ee0630
arnab-mitra <85643543+arnab-mitra@users.noreply.github.com>
2023-03-31 19:18:27 +0530

    EasyJourney Project
    This project is based on a live train tracking system for the Indian Railways.
    I could only make the frontend for this website as I was having difficulty in understanding and implementing the backend. I hope that I'll be able to learn further and make this a full stack website.

---
+++ js/script.js
@@ -0,0 +1,50 @@
+let navbarDiv = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+        navbarDiv.classList.add('navbar-cng');
+    } else {
+        navbarDiv.classList.remove('navbar-cng');
+    }
+});
+
+
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+// show navbar
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw');
+});
+
+// hide side bar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+    }
+});
+
+// stop transition and animatino during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
+
+var btn1 = document.getElementById("btn1");
+
+btn1.addEventListener('click', () => {
+    fetch('https://railway-w6eh.onrender.com/proxy?from=$%7Bfrom%7D&to=$%7Bto%7D&date=$%7Bdate%7D')
+    .then((response) => {
+        return(response.json());
+    })
+    .then((data) => {
+        console.log(data);
+    })
+})
